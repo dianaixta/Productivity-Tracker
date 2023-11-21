d2d09cbc81b57029e0d4aaf56978a7cf17e5761f
Chad Curtis <chad.curtis@nsc.edu>
2023-11-21 08:35:41 -0800

    Fixed dragBox so it turns purple once drawn.

--- dragBox.py
+++ dragBox.py
@@ -4,19 +4,40 @@ win = GraphWin('Draw a box and save coordinates', 400, 400, autoflush=False)
 p1 = None
 rect = None
 
+def samePoint(p1, p2):
+    ptype = type(Point(200, 200))
+    areSame = False
+    if (type(p1) == ptype) & (type(p2) == ptype):
+        sameX = p1.getX() == p2.getX()
+        sameY = p1.getY() == p2.getY()
+        if sameX & sameY:
+            areSame = True
+    return areSame
+
+def currentlyDragging(p1, p2):
+    ptype = type(Point(200, 200))
+    dragging = False
+    if (type(p1) == ptype) & (type(p2) == ptype):
+        sameX = p1.getX() == p2.getX()
+        sameY = p1.getY() == p2.getY()
+        if not (sameX & sameY):
+            dragging = True
+    return dragging
+
 def checkState(p1, p2):
+
     if (p1 == None) & (p2 != None):
         return 'start'
-    elif (p1 != None) & (p2 == None):
+    elif samePoint(p1, p2):
         # Currently never triggers end
         return 'end'
-    elif (p1 == None) & (p2 == None):
-        return 'empty'
-    else:
+    elif currentlyDragging(p1, p2):
         return 'drag'
+    else:
+        return 'empty'
 
 while True:
-    p2 = win.checkDrag()
+    p2 = win.checkMouse()
     if checkState(p1, p2) == 'start':
         pStart = p2.clone()
     elif checkState(p1, p2) == 'drag':
@@ -28,13 +49,17 @@ while True:
     elif checkState(p1, p2) in ['end', 'empty']:
         if p1:
             pEnd = p1.clone()
-            rect.setFill('purple')
-            rect.draw(pStart, pEnd)
-        p1 = None
-    update(10)
-    print(checkState(p1, p2))
+            p1 = None
+            try:
+                rect.setFill('purple')
+                rect.draw(pStart, pEnd)
+            except:
+                pass
+    update(4)
     if p2:
         p1 = p2.clone()
+    else:
+        p1 = p2
 
     # Close window if user hits q
     key = win.checkKey()
