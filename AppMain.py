e0516ca82514b5d7c5b4f52fd6fc34e988e42975
Jahnnah Muhammad <jahnnahm@gmail.com>
2023-12-04 13:12:50 -0800

    Added main file to J's branch

---
+++ AppMain.py
@@ -0,0 +1,17 @@
+from graphics import *
+from mainscreen import mainScreen
+from instructscreen import Instructions
+from dayscreen import DayScreen
+
+
+class ProductivityApp:
+    def __init__(self):
+        instr = Instructions()
+        instr.getChoice()
+
+
+
+
+
+
+ProductivityApp()
